5ab60f71c4bc48a87e758d828bc46d9a519d5c55
AngeloDalo <angelo251158@gmail.com>
2022-05-09 15:30:15 +0200

    for

--- src/App.java
+++ src/App.java
@@ -1,5 +1,37 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       //array multidimensionali
+       String[][] classi = new String[3][3];
+       classi[0][0] = "Luca";
+       classi[0][1] = "Anna";
+       classi[0][2] = "Marco";
+       classi[1][0] = "Luca";
+       classi[1][1] = "Anna";
+       classi[1][2] = "Marco";
+       classi[2][0] = "Luca";
+       classi[2][1] = "Anna";
+       classi[2][2] = "Marco";
+
+       String[][] classi2 = {
+           {"Luca", "Anna", "Marco"},
+           {"Luca", "Anna", "Marco"},
+           {"Luca", "Anna", "Marco"}
+       };
+
+       System.out.println(classi[2][1]);
+
+       for (int classe = 0; classe < classi.length; classe++){
+           System.out.println();
+           for(int studente = 0; studente < classi[classe].length; studente++) {
+               System.out.print(classi[classe][studente] + " ");
+           }
+       }
+
+       for (String[] classe: classi) {
+            System.out.println();
+            for (String studente: classe) {
+                System.out.print(studente + " ");
+            }
+       }
     }
 }
